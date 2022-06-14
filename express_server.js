d3a61da2eea572a835f29266b276f06433415c93
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-06-13 18:54:17 -0600

    tinyApp project 2022-0613-1853

--- express_server.js
+++ express_server.js
@@ -14,12 +14,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-  //res.json(urlDatabase);
-});
-
 app.get("/hello", (req, res) => {
   //res.send("<html><body>Hello <b>World</b></body></html>\n");
 
@@ -28,14 +22,27 @@ app.get("/hello", (req, res) => {
   res.render('hello_world', templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+  //res.json(urlDatabase);
 });
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
+
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+//  });
+ 
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
